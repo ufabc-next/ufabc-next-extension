507ffa9efdb67ccea6eb4bb7d6b73cc0bda3ebd0
Joabesv <joabevarjao123@gmail.com>
2024-01-29 22:37:46 -0300

    fix: manifest json

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,12 +1,18 @@
 chrome.runtime.onMessageExternal.addListener(
-  function(request, sender, sendResponse) {
-    if(request.method == 'storage') {
-      chrome.storage.local.get(request.key, function (item) {
-        item = item || {}
-        sendResponse(item[request.key])
-      })
+  function (request, sender, sendResponse) {
+    if (request.method === "storage") {
+      chrome.storage.local
+        .get(request.key)
+        .then(() => {
+          item = item || {};
+          sendResponse(response);
+        })
+        .catch((error) => {
+          console.log("error on fetch", error);
+          sendResponse(undefined);
+        });
     }
 
-    return true
-  }
-)
+    return true;
+  },
+);
