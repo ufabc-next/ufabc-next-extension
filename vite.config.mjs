5a7b5ffd60e45e84ba16ba26a5a6266a3906e35e
Joabesv <joabevarjao123@gmail.com>
2024-02-09 21:08:24 -0300

    wip: add vite

---
+++ vite.config.mjs
@@ -0,0 +1,53 @@
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import Vue2 from "@vitejs/plugin-vue2";
+
+const EsmDirname = dirname(fileURLToPath(import.meta.url));
+
+const PORT = 6000;
+const resolvePath = (...args) => resolve(EsmDirname, ...args);
+
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
+
+  return {
+    root: resolvePath("src"),
+    base: isDev ? `http://localhost:${PORT}/` : undefined,
+    resolve: {
+      alias: {
+        "@/": `${resolvePath("src")}/`,
+      },
+    },
+    server: {
+      port: PORT,
+      hmr: {
+        host: "localhost",
+      },
+    },
+    build: {
+      outDir: resolvePath("extension/prod"),
+      emptyOutDir: false,
+      sourcemap: isDev ? "inline" : false,
+      rollupOptions: {
+        input: {
+          popup: resolvePath("src/pages/popup.html"),
+        },
+      },
+    },
+    plugins: [
+      Vue2(),
+      {
+        name: "assets-rewrite",
+        enforce: "post",
+        apply: "build",
+        transformIndexHtml(html) {
+          return html.replace(/"\/assets\//g, '"../images/');
+        },
+      },
+    ],
+    optimizeDeps: {
+      include: ["vue", "@vueuse/core"],
+    },
+  };
+});
