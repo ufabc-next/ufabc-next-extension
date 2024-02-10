7be362ec8e59e0363a5a5fe073ec4798a25d17a6
Joabesv <joabevarjao123@gmail.com>
2024-02-09 23:15:20 -0300

    feat: add better logs on terminal

--- tasks/prepare.dev.mjs
+++ tasks/prepare.dev.mjs
@@ -4,11 +4,11 @@ import { execSync } from "node:child_process";
 import { resolve } from "node:path";
 import { watch } from "chokidar";
 import { existsSync } from "node:fs";
-import { EsmDirname } from "./utils.mjs";
+import { EsmDirname, logger } from "./utils.mjs";
 
 const resolvePath = (...args) => resolve(EsmDirname, "..", ...args);
 const PORT = 6000;
-const isDev = process.env.NODE_ENV === "prod";
+const isDev = process.env.NODE_ENV !== "prod";
 
 const ensureDir = async (dir) => {
   if (!existsSync(dir)) {
@@ -17,13 +17,17 @@ const ensureDir = async (dir) => {
 };
 
 async function stubHtml() {
-  const views = ["popup", "matricula"];
+  const views = ["Popup"];
 
   for (const view of views) {
     await ensureDir(resolvePath(`extension/dev/${view}`));
-    let data = await readFile(resolvePath(`src/${view}/index.html`), "utf-8");
+    let data = await readFile(
+      resolvePath(`src/views/${view}/index.html`),
+      "utf-8"
+    );
+
     data = data
-      .replace('"./main.ts"', `"http://localhost:${PORT}/${view}/main.ts"`)
+      .replace('"./main.js"', `"http://localhost:${PORT}/${view}/main.js"`)
       .replace(
         '<div id="app"></div>',
         '<div id="app">Vite server did not start</div>'
@@ -34,7 +38,7 @@ async function stubHtml() {
       data,
       "utf-8"
     );
-    console.log("PRE", `stub ${view}`);
+    logger("PRE", `stub ${view}`);
   }
 }
 
@@ -49,7 +53,7 @@ if (isDev) {
   watch(resolvePath("src/**/*.html")).on("change", () => {
     stubHtml();
   });
-  watch([resolvePath("src/manifest.ts"), resolvePath("package.json")]).on(
+  watch([resolvePath("src/getManifest.mjs"), resolvePath("package.json")]).on(
     "change",
     () => {
       writeManifest();
