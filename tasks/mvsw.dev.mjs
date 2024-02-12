476ab2aa51dbafc051eb0e7f2def44e23c9c0c5b
Joabesv <joabevarjao123@gmail.com>
2024-02-12 19:22:23 -0300

    wip: more dirs to final build

--- tasks/mvsw.dev.mjs
+++ tasks/mvsw.dev.mjs
@@ -7,6 +7,12 @@ import { logger } from "./utils.mjs";
     await cp(resolve("src/assets"), resolve("extension/dev/assets"), {
       recursive: true,
     });
+    await cp(resolve("src/styles"), resolve("extension/dev/styles"), {
+      recursive: true,
+    });
+    await cp(resolve("src/pages"), resolve("extension/dev/pages"), {
+      recursive: true,
+    });
     await rename(
       resolve("extension/dev/dist/background.global.js"),
       resolve("extension/dev/background.js")
@@ -14,6 +20,10 @@ import { logger } from "./utils.mjs";
     await rename(
       resolve("extension/dev/dist/contentscript.global.js"),
       resolve("extension/dev/contentscript.js")
+    );
+    await rename(
+      resolve("extension/dev/dist/contentScriptPortal.global.js"),
+      resolve("extension/dev/contentScriptPortal.js")
     );
     await rm(resolve("extension/dev/dist"), { recursive: true });
     logger("BUILD:SW", "Moved service-worker success!");
--- tasks/mvsw.prod.mjs
+++ tasks/mvsw.prod.mjs
@@ -1,9 +1,16 @@
 import { resolve } from "node:path";
 import { cp, rename, rm } from "node:fs/promises";
+import { logger } from "./utils.mjs";
 
 (async () => {
   try {
     await cp(resolve("src/assets"), resolve("extension/prod/assets"), {
+      recursive: true,
+    });
+    await cp(resolve("src/styles"), resolve("extension/prod/styles"), {
+      recursive: true,
+    });
+    await cp(resolve("src/pages"), resolve("extension/prod/pages"), {
       recursive: true,
     });
     await rename(
@@ -14,8 +21,12 @@ import { cp, rename, rm } from "node:fs/promises";
       resolve("extension/prod/dist/contentscript.global.js"),
       resolve("extension/prod/contentscript.js")
     );
+    await rename(
+      resolve("extension/prod/dist/contentScriptPortal.global.js"),
+      resolve("extension/prod/contentScriptPortal.js")
+    );
     await rm(resolve("extension/prod/dist"), { recursive: true });
-    console.log("sucessfuly built worker [PROD]");
+    logger("sucessfuly built worker [PROD]");
   } catch (error) {
     console.error("error moving files [PROD]", error);
   }
