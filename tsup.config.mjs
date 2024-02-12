690a7e7076828316e74d4fb2e376ec1a279cb9ec
Joabesv <joabevarjao123@gmail.com>
2024-02-12 19:20:45 -0300

    feat: better tsup config

--- tsup.config.mjs
+++ tsup.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from "tsup";
 const isDev = process.env.NODE_ENV !== "prod";
 
 export default defineConfig({
-  entry: ["src/scripts/background.js", "src/scripts/contentscript.js"],
+  entry: [
+    "src/scripts/background.js",
+    "src/scripts/contentscript.js",
+    "src/scripts/contentScriptPortal.js",
+  ],
   target: "node20",
   format: "iife",
   outDir: isDev ? "extension/dev/dist" : "extension/prod/dist",
   splitting: false,
+  minify: isDev ? false : true,
+  env: {
+    NODE_ENV: isDev ? "dev" : "prod",
+  },
 });
