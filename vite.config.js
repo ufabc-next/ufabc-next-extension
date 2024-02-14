190fba1a6fae59fa26abf40b9dfc415081eea13c
Joabesv <joabevarjao123@gmail.com>
2024-02-14 09:44:27 -0300

    chore: unify build on vite

--- vite.config.js
+++ vite.config.js
@@ -1,61 +1,68 @@
 import { resolve, dirname, relative } from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import Vue2 from "@vitejs/plugin-vue2";
 import Components from "unplugin-vue-components/vite";
+import packageJson from './package.json' with { type: "json" }
+import { isDev, PORT, EsmDirname } from './tasks/utils';
 
-const EsmDirname = dirname(fileURLToPath(import.meta.url));
-
-const PORT = 5001;
 const resolvePath = (...args) => resolve(EsmDirname, ...args);
 
-export default defineConfig(({ command }) => {
-  const isDev = command === "serve";
+/**@type {import('vite').UserConfig;} */
+export const sharedViteConfig = {
+  root: resolvePath("src"),
+  resolve: {
+    alias: {
+      "@/": `${resolvePath("src")}/`,
+    },
+  },
+  plugins: [
+    Vue2(),
+    Components({
+      dirs: [resolvePath("src/components")],
+      dts: false,
+    }),
+    {
+      name: "assets-rewrite",
+      enforce: "post",
+      apply: "build",
+      transformIndexHtml(html, { path }) {
+        return html.replace(
+          /"\/assets\//g,
+          `"${relative(dirname(path), "/assets")}/`
+        );
+      },
+    },
+  ],
+  optimizeDeps: {
+    include: ["vue", "@vueuse/core"],
+  },
+  define: {
+    __DEV__: isDev,
+    __NAME__: JSON.stringify(packageJson.name),
+  },
+};
 
-  return {
-    root: resolvePath("src"),
-    base: isDev ? `http://localhost:${PORT}/` : undefined,
-    resolve: {
-      alias: {
-        "@/": `${resolvePath("src")}/`,
+export default defineConfig(({ command }) => ({
+  ...sharedViteConfig,
+  base: command === "serve" ? `http://localhost:${PORT}/` : "/dist/",
+  server: {
+    port: PORT,
+    hmr: {
+      host: "localhost",
+    },
+  },
+  build: {
+    watch: isDev ? {} : undefined,
+    outDir: resolvePath("extension/dist"),
+    emptyOutDir: false,
+    sourcemap: isDev ? "inline" : false,
+    terserOptions: {
+      mangle: false,
+    },
+    rollupOptions: {
+      input: {
+        popup: resolvePath("src/views/Popup/index.html"),
       },
     },
-    server: {
-      port: PORT,
-      hmr: {
-        host: "localhost",
-      },
-    },
-    build: {
-      outDir: resolvePath("extension/prod"),
-      emptyOutDir: false,
-      sourcemap: isDev ? "inline" : false,
-      rollupOptions: {
-        input: {
-          popup: resolvePath("src/views/Popup/index.html"),
-        },
-      },
-    },
-    plugins: [
-      Vue2(),
-      Components({
-        dirs: [resolvePath("src/components")],
-        dts: false,
-      }),
-      {
-        name: "assets-rewrite",
-        enforce: "post",
-        apply: "build",
-        transformIndexHtml(html, { path }) {
-          return html.replace(
-            /"\/assets\//g,
-            `"${relative(dirname(path), "/assets")}/`
-          );
-        },
-      },
-    ],
-    optimizeDeps: {
-      include: ["vue", "@vueuse/core"],
-    },
-  };
-});
+  },
+}));
